3e51e23a89c09998b2dbc81345669b07350860d7
Alex Taylor <aptaylor87@gmail.com>
2024-02-26 09:11:38 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import JokeList from "./JokeList";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  )
+}
+
+export default App
